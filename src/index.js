e9e0c766fa8f47c4bebc1c28a5681838e55ccf03
Mateusz Woźniak <crow.mw@gmail.com>
2017-08-31 22:06:12 +0200

    Disable webpack-devtools on production

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import thunk from 'redux-thunk'
 
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  process.env.NODE_ENV !== 'production' &&
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 )
 

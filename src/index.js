bd16e91911f61d3769bba7a4940253ec357f9155
Mateusz Woźniak <crow.mw@gmail.com>
2017-08-31 20:12:08 +0200

    Add react-redux and simple store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import './index.css'
 import Search from './components/search/search'
-// import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Search />, document.getElementById('root'))
-// registerServiceWorker()
+import reducers from './reducers'
+import thunk from 'redux-thunk'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Search />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
